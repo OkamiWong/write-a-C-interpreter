6078108072b7efdadaed56f4e10a7b1669d81289
Okami Wong <wtfimba@live.com>
2020-01-25 13:54:02 +0800

    add comments

--- c_interpreter.c
+++ c_interpreter.c
@@ -3,10 +3,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-int token;
-char *src, *old_src;
-int poolsize;
-int line;
+int token;//current token
+char *src, *old_src;//pointer to sourcecode string
+int poolsize;//default size of text/data/stack
+int line;//line number
 
 void next() {
     token = *src++;
@@ -18,7 +18,7 @@ void expression(int level) {
 }
 
 void program() {
-    next();
+    next();//get next token
     while (token > 0) {
         printf("token is: %c\n", token);
         next();
@@ -36,23 +36,27 @@ int main(int argc, char **argv) {
     poolsize = 256 * 1024;
     line = 1;
 
+    //open source file
     if ((fd = open(*argv, 0)) < 0) {
         printf("could not open(%s)\n", *argv);
         return -1;
     }
 
+    //malloc memory
     if (!(src = old_src = malloc(poolsize))) {
         printf("could not malloc(%d) for source area\n", poolsize);
         return -1;
     }
 
+    //read source file
     if ((i = read(fd, src, poolsize - 1)) <= 0) {
         printf("read() returned %d\n", i);
         return -1;
     }
 
-    src[i] = 0;
+    src[i] = 0;//add EOF
     close(fd);
+
 
     program();
     return eval();
