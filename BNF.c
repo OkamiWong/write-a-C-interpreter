7afa5bd7fabb4dbf9c5e1547a903cfcfd6e5d4e4
Okami Wong <wtfimba@live.com>
2020-01-26 18:07:39 +0800

    finish BNF

--- BNF.c
+++ BNF.c
@@ -1,3 +1,4 @@
+// must be compiled in Linux
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,6 +7,33 @@ int token;
 int token_val;
 char *line = NULL;
 char *src = NULL;
+
+void next() {
+    // skip white space
+    while (*src == ' ' || *src == '\t') {
+        src++;
+    }
+
+    token = *src++;
+
+    if (token >= '0' && token <= '9') {
+        token_val = token - '0';
+        token = Num;
+        while (*src >= '0' && *src <= '9') {
+            token_val = token_val * 10 + *src - '0';
+            src++;
+        }
+        return;
+    }
+}
+
+void match(int tk) {
+    if (token != tk) {
+        printf("expected token: %d(%c), got %d(%c)\n", tk, tk, token, token);
+        exit(-1);
+    }
+    next();
+}
 
 int expr();
 
@@ -60,36 +88,9 @@ int expr() {
     return expr_tail(lvalue);
 }
 
-void next() {
-    // skip white space
-    while (*src == ' ' || *src == '\t') {
-        src++;
-    }
-
-    token = *src++;
-
-    if (token >= '0' && token <= '9') {
-        token_val = token - '0';
-        token = Num;
-        while (*src >= '0' && *src <= '9') {
-            token_val = token_val * 10 + *src - '0';
-            src++;
-        }
-        return;
-    }
-}
-
-void match(int tk) {
-    if (token != tk) {
-        printf("expected token: %d(%c), got %d(%c)\n", tk, tk, token, token);
-        exit(-1);
-    }
-    next();
-}
-
 int main(int argc, char *argv[]) {
     size_t linecap = 0;
-    size_t linelen;
+    ssize_t linelen;
     while ((linelen = getline(&line, &linecap, stdin)) > 0) {
         src = line;
         next();
